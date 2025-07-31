78daccf173741f8f3d4e226cc204e7d87a988f59
Akshay Negi <akshaynegi18@gmail.com>
2025-07-31 21:00:36 +0530

    code working in localhost..

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -10,37 +10,39 @@ namespace UserService
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // Ensure the Microsoft.EntityFrameworkCore.SqlServer package is installed
+            
+
             builder.Services.AddDbContext<UserDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
-            // Apply database migrations at startup.
+            // Apply database migrations with retry logic
             using (var scope = app.Services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetRequiredService<UserDbContext>();
-                db.Database.Migrate();
-            }
+                            db.Database.Migrate();
+                    }
 
             // Configure the HTTP request pipeline.
            
                 app.UseSwagger();
                 app.UseSwaggerUI();
-           
 
-            app.UseHttpsRedirection();
+
+                app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-
+           
             app.MapControllers();
+
+            var logger = app.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("UserService starting in {Environment} environment on ports 8081", app.Environment.EnvironmentName);
 
             app.Run();
         }
